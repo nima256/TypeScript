87acfb170aeaac9da0c89bcdecbf0996a96d04e6
Nima <www.nimamahdi1385@gmail.com>
2023-11-03 19:16:33 +0330

    Arrays in TypeScript

---
+++ arrays.ts
@@ -0,0 +1,17 @@
+const activeUser: string[] = ["Nima"];
+activeUser.push('aajsdlk;fa');
+
+const ageList: number[] = [55, 478, 52];
+ageList[0] = 752;
+
+// we can create array like this too
+const bools: Array<boolean> = [true];
+const bools2: boolean[] = [false];
+
+// nested array 
+const board: string[][] = [
+    ['x', 'O', 'x'],
+    ['x', 'O', 'x'],
+    ['x', 'O', 'x'],
+    ['x', 'O', 'x']
+]
