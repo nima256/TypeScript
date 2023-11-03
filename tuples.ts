6076cce87c803a4c5d0546aeb5adb05ffe66c995
Nima <www.nimamahdi1385@gmail.com>
2023-11-03 21:23:44 +0330

    tuples in TypeScript

---
+++ tuples.ts
@@ -0,0 +1,8 @@
+// rgb color
+// just allowed three number
+const color : [number , number , number] = [54 , 47 , 22];
+
+type HTTPResponse = [number , string];
+
+const goodRes : HTTPResponse = [20 , "Good"]
+// const goodRes2 : HTTPResponse = ["Good" , 20]; Error because the order
